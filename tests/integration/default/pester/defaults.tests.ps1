e507edba04b75623ecbc8c96cce5c829a2a7724d
Avishay Bar <avishay.bar@cyberark.com>
2018-12-31 16:00:55 +0000

    - Completed converge with kitchen

--- tests/integration/default/pester/defaults.tests.ps1
+++ tests/integration/default/pester/defaults.tests.ps1
@@ -1,16 +1,8 @@
 describe "ansible_test_kitchen_windows_role ansible role" {
-    Context "windows programs" {
-        $programsToGet = @(
-            'Google Chrome'
-        )
-
-        $x = Get-ChildItem "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-
-        ForEach ($f in $programsToGet)
-        {
-            it "$($f) program is installed" {
-                ($x | Where-Object { $_.GetValue( "DisplayName" ) -like $f }) | Should Be $true
-            }
+    Context "PSM Installation Path" {
+        $Path = "C:\Program Files (x86)\Cyberark\PSM\Recordings"
+        it "PSM Recordings Directory Exists" {
+            Test-Path -Path $Path | Should be $true
         }
     }
 }
--- tests/scripts/user_data_windows_2016.ps1
+++ tests/scripts/user_data_windows_2016.ps1
@@ -1,36 +1,13 @@
 <powershell>
+# Change computer password
+$admin = [adsi]("WinNT://./administrator, user")
+$admin.PSBase.Invoke("SetPassword", "Kitchen123$")
+
 # Configure machine for ansible remoting
-$logfile="C:\ProgramData\Amazon\EC2-Windows\Launch\Log\kitchen-ec2.log"
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file -EnableCredSSP
 
-# Allow script execution
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-
-# PS Remoting and & winrm.cmd basic config
-Enable-PSRemoting -Force -SkipNetworkProfileCheck
-winrm set winrm/config '@{MaxTimeoutms="1800000"}' >> $logfile
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="1024"}' >> $logfile
-winrm set winrm/config/winrs '@{MaxShellsPerUser="50"}' >> $logfile
-
-# Server settings - support username/password login
-winrm set winrm/config/service/auth '@{Basic="true"}' >> $logfile
-winrm set winrm/config/service '@{AllowUnencrypted="true"}' >> $logfile
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="1024"}' >> $logfile
-
-#Firewall Config
-netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" profile=public protocol=tcp localport=5985 remoteip=localsubnet new remoteip=any  >> $logfile
-
-# Disable Complex Passwords
-"Disabling Complex Passwords" >> $logfile
-$seccfg = [IO.Path]::GetTempFileName()
-secedit /export /cfg $seccfg
-(Get-Content $seccfg) | Foreach-Object {$_ -replace "PasswordComplexity\s*=\s*1", "PasswordComplexity=0"} | Set-Content $seccfg
-secedit /configure /db $env:windir\security\new.sdb /cfg $seccfg /areas SECURITYPOLICY
-del $seccfg
-
-# Change Password
-"Change Password" >> $logfile
-$ComputerName = $env:COMPUTERNAME
-$user = [adsi]"WinNT://$ComputerName/Administrator,user"
-$user.setpassword("Kitchen")
 Set-ItemProperty -Name LocalAccountTokenFilterPolicy -Path HKLM:\software\Microsoft\Windows\CurrentVersion\Policies\system -Value 1
 </powershell>
